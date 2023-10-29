0dc0b8bdbb134db3b3abfb020955fc4c0a0a8a84
muzammilk03 <muzammilkappan@gmail.com>
2023-10-29 14:12:38 +0530

    feat:tabs work done

--- components/ui/Tabs.jsx
+++ components/ui/Tabs.jsx
@@ -3,26 +3,26 @@ import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
+import useTabStore from "@/libs/stores/useTab";
 
-export default function TabPanel() {
-  const [value, setValue] = React.useState("one");
+export default function TabsPanel() {
+  const { setState, tabValue } = useTabStore();
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    setState({ tabValue: newValue });
   };
-
   return (
     <Box sx={{ width: "100%" }}>
       <Tabs
-        value={value}
+        value={tabValue}
         onChange={handleChange}
         textColor="primary"
         indicatorColor="secondary"
         aria-label="secondary tabs example"
       >
-        <Tab value="one" label="Home" />
-        <Tab value="two" label="Best Matches" />
-        <Tab value="three" label="Favourites" />
+        <Tab value="one" label="Home" sx={{ fontWeight: "bold" }} />
+        <Tab value="two" label="Best Matches" sx={{ fontWeight: "bold" }} />
+        <Tab value="three" label="Favourites" sx={{ fontWeight: "bold" }} />
       </Tabs>
     </Box>
   );
