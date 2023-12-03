a15db5989c3fcc945a0a7603c7500337a7444ba6
Shabin-k <helloshabink@gmail.com>
2023-12-03 21:54:21 +0530

    feat(test): new things
    hahahahhahaha
    
    BREAKING CHANGE: hahaksdfjaskdjfjaslkdf
    jdfs

--- libs/stores/useTab.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useTabStore = create((set) => ({
-  tabValue: "one",
-  setState: (newState) => set({ tabValue: newState.tabValue }),
-}));
-
-export default useTabStore;
