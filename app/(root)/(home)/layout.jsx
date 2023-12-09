0b4b54e684dceaba9a0f50bc9b8b32f6771fc856
Shabin-k <helloshabink@gmail.com>
2023-12-09 11:50:53 +0530

    feat (jsj): add new things

--- app/(root)/(home)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function HomeLayout({ children }) {
-  return (
-    <section className="relative flex flex-col p-2">
-      <main className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">{children}</main>
-    </section>
-  );
-}
--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>home</div>;
+  return <main className=" mx-auto w-full max-w-7xl overflow-hidden px-4 pb-16">home</main>;
 }
