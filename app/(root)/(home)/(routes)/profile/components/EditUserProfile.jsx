e31afd2289923ec42b6598522e2e510e17776890
Shabin-k <helloshabink@gmail.com>
2023-12-16 12:20:27 +0530

    STYLE : improve style

--- app/(root)/(home)/(routes)/profile/components/EditUserProfile.jsx
+++ app/(root)/(home)/(routes)/profile/components/EditUserProfile.jsx
@@ -239,17 +239,17 @@ const UserPostedJobs = () => {
           </div>
         </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
+        <div className="mt-6 flex items-center gap-x-6">
           <button
             onClick={handleClear}
             type="button"
-            className="text-sm font-semibold leading-6 text-gray-900"
+            className="rounded-md py-2 text-sm font-semibold leading-6 text-gray-900"
           >
             Cancel
           </button>
           <button
             onClick={handleSubmit}
-            type="submit"
+            type="button"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Save
