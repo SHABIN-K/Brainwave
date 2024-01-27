e40626721182d1f6895af911e95cca570719bcc8
Shabin-k <helloshabink@gmail.com>
2024-01-27 12:03:32 +0530

    fix

--- app/(root)/(home)/(routes)/profile/components/UserPostedJobs.jsx
+++ app/(root)/(home)/(routes)/profile/components/UserPostedJobs.jsx
@@ -9,7 +9,7 @@ const UserPostedJobs = ()=>{
     <main className=" mx-auto grid h-full w-full max-w-7xl gap-6 px-4 pb-16 md:grid-cols-[auto_1fr] md:py-10 ">
     <div className=" top-11 z-10 grid h-fit gap-6 bg-light-500 md:sticky  md:max-w-[250px] "></div>
     <div className=" z-10">
-      <ul className="grid gap-6 "></ul>
+    <ul className="grid gap-6 ">{skeleton}</ul>
     </div>
   </main>
   );
