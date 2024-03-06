cac39cf27a0763438afcc05a2d20bf91c305f1d3
Shabin-k <helloshabink@gmail.com>
2024-03-06 23:12:55 +0530

    feat : add new styles

---
+++ components/landing/Tagline.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import brackets from "../../public/assets/svg/Brackets";
+
+interface TagLineProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const TagLine: FC<TagLineProps> = ({ className, children }) => {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {brackets("left")}
+      <div className="mx-3 text-n-3">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+};
+
+export default TagLine;
