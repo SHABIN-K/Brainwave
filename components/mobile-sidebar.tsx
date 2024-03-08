1c00f5573e3dfb8fbe7740cd813e30beb4952cb6
Shabin-k <helloshabink@gmail.com>
2024-03-08 21:57:27 +0530

    refator : delete

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
 
+import { Sidebar } from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Sidebar } from "@/components/sidebar";
 
 export const MobileSidebar = ({
   apiLimitCount = 0,
